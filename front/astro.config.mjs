92e099f9624a0c9a584f5181f55305a1f21701eb
yare <ejimtpck@gmail.com>
2026-04-08 10:01:11 +0800

    調整版面配置、新增登入頁面(還沒詳讀)

--- front/astro.config.mjs
+++ front/astro.config.mjs
@@ -1,10 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
+import cloudflare from '@astrojs/cloudflare';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'hybrid',
+  adapter: cloudflare({
+    platformProxy: {
+      enabled: true
+    }
+  }),
   vite: {
     plugins: [tailwindcss()]
   }
