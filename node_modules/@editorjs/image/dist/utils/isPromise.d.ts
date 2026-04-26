039e59c27c1a67e436cd5eba0f1cd209c0ed5f8b
yare <ejimtpck@gmail.com>
2026-04-26 15:52:41 +0800

    再來看看

--- node_modules/@editorjs/image/dist/utils/isPromise.d.ts
+++
@@ -1,8 +0,0 @@
-import { UploadResponseFormat } from '../types/types';
-
-/**
- * Check if passed object is a Promise
- * @param  object - object to check
- * @returns
- */
-export default function isPromise(object: Promise<UploadResponseFormat>): object is Promise<UploadResponseFormat>;
